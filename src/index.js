3ec53a79ec02d9091406970c3975367a85278815
mjLee <audwn0326@gmail.com>
2021-12-20 11:29:32 +0900

    add github page deploy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import "@fortawesome/fontawesome-free/js/all.js";
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./app";
 import "./app.css";
-import SimpleHabit from "./components/simpleHabit";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <SimpleHabit />
+    <App />
+    {/* <SimpleHabit /> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
