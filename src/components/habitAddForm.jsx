3f7258a4fab6c0c9c62e073d5daf226f457f183d
mjLee <audwn0326@gmail.com>
2022-01-05 09:58:29 +0900

    remove class component

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -25,31 +25,3 @@ const HabitAddForm = memo((props) => {
 });
 
 export default HabitAddForm;
-// import React, { PureComponent } from "react";
-
-// export default class HabitAddForm extends PureComponent {
-//   formRef = React.createRef();
-//   inputRef = React.createRef();
-//   onSubmit = (event) => {
-//     event.preventDefault();
-//     const name = this.inputRef.current.value;
-//     name && this.props.onAdd(name);
-//     this.formRef.current.reset();
-//   };
-
-//   render() {
-//     return (
-//       <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
-//         <input
-//           ref={this.inputRef}
-//           className="add-input"
-//           type="text"
-//           placeholder="Habit"
-//         />
-//         <button className="add-button" onClick={this.onClickAdd}>
-//           Add
-//         </button>
-//       </form>
-//     );
-//   }
-// }
